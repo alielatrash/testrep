2a66eb730a4dcb4361a99c4578b0802cae3e8753
alielatrash <elatrash.ali@gmail.com>
2017-10-29 16:23:43 +0200

    Add files via upload

---
+++ ContactCars.py
@@ -0,0 +1,51 @@
+import urllib2
+import bs4
+import pandas as pd
+
+makes = 3
+pages = 3
+
+for make in range(1,makes):
+    for page in range(1,pages):
+        theurl = 'https://www.contactcars.com/usedcars?search=uc&mk=%s&page=%s' % (make, page)
+
+        headers = "make, model, year, price, km, cc\n"
+
+        data_set = []
+
+        for url in theurl:
+            thepage = urllib2.urlopen(theurl)
+            soup = bs4.BeautifulSoup(thepage, "html.parser")
+
+            g_data = soup.findAll('div', {"class":"panel custom_p_2"})
+            z_data = soup.findAll('div', {"class": "large-9 medium-9 small-9 columns"})
+
+            i = 0
+
+            for item in g_data:
+                print i
+                data_2 = g_data[i].div
+                make = data_2.findAll('p', {"class":"tit_2"})[0].text.encode('utf-8')
+                model = data_2.findAll('p', {"class": "tit_3 right"})[0].text.encode('utf-8')
+                year = data_2.findAll('p', {"class": "tit_3 right"})[1].text.encode('utf-8')
+                price = data_2.findAll('span', {"class": "orange_txt left"})[0].text.encode('utf-8')
+
+                for data in z_data:
+                    data_3 = z_data[i]
+                    cc = data_3.findAll('p', {"class": "right tit_3"})[0].text.encode('utf-8')
+                    km = data_3.findAll('span', {"class": "right tit_2"})[0].text.encode('utf-8')
+
+                i = i + 1
+
+                # data_set.append((make, model, year, price, km, cc))
+
+                print make
+                print model
+                print year
+                print price
+                print cc
+                print km
+
+                    # data_frame = pd.DataFrame(data_set, columns = [make, model, year, price, km, cc])
+                    #
+                    # data_frame.to_csv('contact_cars.csv', index = False, encoding = 'utf-8')
